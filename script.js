1e77b2a0b537ba27cd9f218b3d4ddf8c2f3b8640
aristos88 <117604228+aristos88@users.noreply.github.com>
2022-11-07 21:29:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener("touchmove", preventMotion, false);
 
 function preventMotion(event)
 {
-    window.scrollTo(0, 0);
+    window.scrollTo(10, 10);
     event.preventDefault();
     event.stopPropagation();
 }
